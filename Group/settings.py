fb027b45739f6a0d65314d1e5799f7af3d31eaca
Shriganesh Bhide <shriganesh2019@iiitkottayam.ac.in>
2022-12-26 18:42:41 +0530

    created dynamic binding in home page

--- Group/settings.py
+++ Group/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'Group.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- base/views.py
+++ base/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+rooms = [
+    {'id':1 , 'name': "This is first id"},
+    {'id':2 , 'name': "This is second id"},
+    {'id':3 , 'name': "This is third id"},
+]
 def home(request):
-    return HttpResponse('Home page')
+    context =   {'rooms' : rooms}
+    return render(request , 'home.html' ,context )
 
 def room(request):
-    return HttpResponse("Rooms page")
+    return render(request, 'room.html')
 
 
