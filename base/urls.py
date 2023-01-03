5beaf13668e06b6f48398cd988a994b93fd84014
Shriganesh Bhide <shriganesh2019@iiitkottayam.ac.in>
2023-01-03 19:46:38 +0530

    learned to create a dynamic search bar , and corrected the css , next step is user authentication

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path , include
 from . import views
 urlpatterns = [
+    path('login/', views.loginPage, name = "login"),
     path('', views.home , name = "home"),
     path('room/<str:pk> ', views.room , name = "room"),
     path('create-room/', views.createRoom , name = "create-room"),
--- base/views.py
+++ base/views.py
@@ -1,20 +1,58 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from .models import Room , Topic
+
+from django.contrib import messages
 # Create your views here.
+
+from django.db.models import Q #helps for dynamic search , we can have || or && in if statements
 from .forms import RoomForm
 
+
+from django.contrib.auth import authenticate , login, logout
 # rooms = [
 #     {'id':1 , 'name': "This is first id"},
 #     {'id':2 , 'name': "This is second id"},
 #     {'id':3 , 'name': "This is third id"},
 # ]
 
+def loginPage(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        
+        try:
+           user = User.objects.get(username = username)
+        except:
+            messages.error(request, "No such user")
+        user = authenticate(request , username=username , password=password)
+
+        if user is not None:
+            login(request , user)
+            return redirect('home')
+        else:
+            messages.error(request, "username or password doesn't exist")
+    
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q')!=None else ''
+    
+    
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    
+    room_count = rooms.count()
     topics = Topic.objects.all()
-    context =   {'rooms' : rooms , 'topics' : topics}
+    context =   {'rooms' : rooms , 'topics' : topics  ,'room_count':room_count}
     return render(request , 'base/home.html' ,context)
 
 def room(request, pk):
