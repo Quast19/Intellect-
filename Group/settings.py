ce71eecd4616ae5d6a88e293bd74e7a735d4e125
Shriganesh Bhide <shriganesh2019@iiitkottayam.ac.in>
2023-01-14 23:25:07 +0530

    Default user model commit , instead of merge

--- Group/settings.py
+++ Group/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t2g=n+0jdb0)eh=ewuh$5bv)b80gy8kb589ett#4y&=bth5)sp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 
